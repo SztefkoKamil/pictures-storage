202c1bd16812024a0e814ed14f927c2cfd26b346
Kamil Sztefko <sztefkokamil@gmail.com>
2019-01-30 23:06:54 +0100

    images container DOM creation ADD

--- js/storage.js
+++ js/storage.js
@@ -103,19 +103,59 @@ function showPictures(data){
   for(let i=0; i<data.length; i++){
     const newContainer = document.createElement('div');
     newContainer.classList.add('img-container');
+    newContainer.setAttribute("data-id", data[i]["id"]);
 
     const newImg = document.createElement('img');
     newImg.classList.add('image');
     newImg.setAttribute("src", "data:image;base64,"+data[i]["img"]);
-    newImg.setAttribute("data-id", data[i]["id"]);
 
-    // const newOptionsContainer = document.createElement('div');
+    const newLayout = document.createElement('div');
+    newLayout.classList.add("layout");
 
-    newContainer.appendChild(newImg)
+    const options = [];
 
-    document.querySelector('#storage-container').appendChild(newContainer);
+    const newDloadBtn = document.createElement('button');
+    newDloadBtn.classList.add("download-img-button");
+    newDloadBtn.id = "download-img-button";
+    newDloadBtn.innerText = "DL";
+    options.push(newDloadBtn);
+
+    const newDeleteBtn = document.createElement('button');
+    newDeleteBtn.classList.add("delete-img-button");
+    newDeleteBtn.id = "delete-img-button";
+    newDeleteBtn.innerText = "DE";
+    options.push(newDeleteBtn);
+    
+    const newImgName = document.createElement('h5');
+    newImgName.classList.add("img-name");
+    newImgName.id = "img-name";
+    newImgName.innerText = "Nazwa zdjęcia";
+    options.push(newImgName);
+
+    const newEditBtn = document.createElement('button');
+    newEditBtn.classList.add("edit-img-button");
+    newEditBtn.id = "edit-img-button";
+    newEditBtn.innerText = "E";
+    options.push(newEditBtn);
+
+    for(let i of options){
+      newLayout.appendChild(i);
+    }
+
+    newContainer.appendChild(newImg);
+    newContainer.appendChild(newLayout);
+
+    storageContainer.appendChild(newContainer);
+  
   }
+
 } // ----- showPictures function -------------------
+
+function doRequest(data){
+
+  // fetch().then()
+
+}
 
 
 
